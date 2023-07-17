37613ae4491cd34a4cb976a1f2434e6169246601
niksi99 <104139232+niksi99@users.noreply.github.com>
2023-07-17 21:02:55 +0200

    хешд пасворд

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const {isEmail} = require('validator')
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     email: {
@@ -15,6 +16,18 @@ const userSchema = new mongoose.Schema({
         minLength: [6, "Minimum password length is 6 chars"]
     },
 })
+
+//fire functafter new document is saved tot db
+// userSchema.post('save', function (justSavedDocument, next) {
+//     console.log('New user iscreated', justSavedDocument)
+//     next();
+// })
+
+userSchema.pre('save', async function(next) {
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  });
 
 const User = mongoose.model('user', userSchema)
 
